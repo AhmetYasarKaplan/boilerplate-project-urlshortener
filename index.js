38ff9dcc8bf47b756217b6b142f03035e42f2935
AhmetYasarKaplan <e210503060@stud.tau.edu.tr>
2024-09-03 22:38:10 +0300

    solution to the url shortener project without db

--- index.js
+++ index.js
@@ -1,24 +1,68 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const urlparser = require("url");
+const dns = require("dns");
 const app = express();
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+let idCounter = 1;
+const mapUrl = {};
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+//check if URL is valid
+// const isValidUrl = (url) => {
+//   try {
+//     const validProtocols = ["http:", "https:"];
+//     const urlObject = new URL(url); // This will throw if the URL is invalid
+
+//     return validProtocols.includes(urlObject.protocol);
+//   } catch (error) {
+//     return false;
+//   }
+// };
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", (req, res) => {
+  console.log(req.body);
+
+  const orgURL = req.body.url;
+  console.log(orgURL);
+  const dnslookup = dns.lookup(
+    urlparser.parse(orgURL).hostname,
+    (err, address) => {
+      if (!address) {
+        res.json({ error: "invalid url" });
+      } else {
+        const shortUrlId = idCounter++;
+        mapUrl[shortUrlId] = orgURL;
+        res.json({ original_url: orgURL, short_url: shortUrlId });
+      }
+    }
+  );
+});
+
+app.get("/api/shorturl/:url", (req, res) => {
+  const inputShortUrl = parseInt(req.params.url);
+  const foundUrl = mapUrl[inputShortUrl];
+  console.log("db: ", mapUrl);
+
+  res.redirect(foundUrl);
+});
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
