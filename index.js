4dd398f41c71a8c10b765f03e9d78a25e79c7105
AhmetYasarKaplan <e210503060@stud.tau.edu.tr>
2024-09-04 12:36:18 +0300

    add mongodb connection to store urls

--- index.js
+++ index.js
@@ -4,7 +4,37 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const urlparser = require("url");
 const dns = require("dns");
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: Number,
+});
+
+const Url = mongoose.model("Url", urlSchema);
+
+const createAndSaveUrl = (url, done) => {
+  // Find the highest short_url in the database
+  Url.findOne()
+    .sort({ short_url: -1 })
+    .exec((err, result) => {
+      const newShortUrl = result ? result.short_url + 1 : 1;
+      const newUrl = new Url({
+        original_url: url,
+        short_url: newShortUrl,
+      });
+      newUrl.save((err, savedUrl) => {
+        if (err) return done(err);
+        done(null, savedUrl);
+      });
+    });
+};
 
 // Basic Configuration
 const port = process.env.PORT;
@@ -12,9 +42,6 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static(`${process.cwd()}/public`));
-
-let idCounter = 1;
-const mapUrl = {};
 
 //check if URL is valid
 // const isValidUrl = (url) => {
@@ -48,20 +75,35 @@ app.post("/api/shorturl", (req, res) => {
       if (!address) {
         res.json({ error: "invalid url" });
       } else {
-        const shortUrlId = idCounter++;
-        mapUrl[shortUrlId] = orgURL;
-        res.json({ original_url: orgURL, short_url: shortUrlId });
+        Url.findOne({ original_url: orgURL }).exec((err, existingUrl) => {
+          if (existingUrl) {
+            return res.json({
+              original_url: existingUrl.original_url,
+              short_url: existingUrl.short_url,
+            });
+          }
+          createAndSaveUrl(orgURL, (err, savedUrl) => {
+            if (err) return res.json({ error: "Error saving URL" });
+            res.json({
+              original_url: savedUrl.original_url,
+              short_url: savedUrl.short_url,
+            });
+          });
+        });
       }
     }
   );
 });
 
-app.get("/api/shorturl/:url", (req, res) => {
-  const inputShortUrl = parseInt(req.params.url);
-  const foundUrl = mapUrl[inputShortUrl];
-  console.log("db: ", mapUrl);
+app.get("/api/shorturl/:id", (req, res) => {
+  const inputShortUrl = parseInt(req.params.id);
 
-  res.redirect(foundUrl);
+  Url.findOne({ short_url: inputShortUrl }, (err, foundUrl) => {
+    if (err || !foundUrl) {
+      return res.json({ error: "No short URL found for the given input" });
+    }
+    res.redirect(foundUrl.original_url);
+  });
 });
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
